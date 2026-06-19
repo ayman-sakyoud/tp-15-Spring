ae865b4397166ee60822ab83a9a17511ee3c00bd
ayman147-cmk <aymansakyoud321@gmail.com>
2026-06-20 00:51:38 +0100

    premier commit

---
+++ src/main/java/org/example/tp15spring/Tp15SpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp15spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp15SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp15SpringApplication.class, args);
+    }
+
+}
